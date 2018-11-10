029dc64baf02feb81fe98b988e930fb013185d42
David Zhang <cajan2@163.com>
2018-11-10 11:50:01 +0800

    5.2 finished

--- imooc/settings.py
+++ imooc/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 sys.path.insert(0, os.path.join(BASE_DIR, 'imooc'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -134,3 +135,8 @@ USE_TZ = False  # 是否utc时间（数据库模型）
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'common_static'),)
+
+try:
+    from .settings_local import *
+except Exception as e:
+    print(e)
--- imooc/urls.py
+++ imooc/urls.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
+
 import xadmin
+from xadmin.plugins import xversion
 
 xadmin.autodiscover()
 
 # version模块自动注册需要版本控制的 Model
-from xadmin.plugins import xversion
 
 xversion.register_models()
 
